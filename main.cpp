8748b5bf23a70e132d9451eded585cf597aec4d0
Mạnh Hùng <162700816+mhung1224@users.noreply.github.com>
2024-07-16 01:13:52 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include "Classes.h"
 #include "ExFuncs.h"
 
-
 int main() {
-
     Problem Prob;
     Prob.add_Problem();
     Prob.display(1);
-    S2P_Method S2P(Prob);
-    S2P.solve();
+    S2P_Method S2P;
+    S2P.solve(Prob);
     S2P.display();
     return 1;
 }
